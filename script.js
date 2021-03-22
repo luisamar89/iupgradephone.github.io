7cfc88a59645d97b4dc044c313e8f65e3d404178
luisamar89 <75043731+luisamar89@users.noreply.github.com>
2021-03-22 00:14:36 +0000

    changed

--- script.js
+++ script.js
@@ -17,5 +17,29 @@ function topFunction() {
   document.documentElement.scrollTop = 0; // Chrome, Firefox, IE and Opera
 }
 
+
+/*-----------------------*/
+
+
+function initMap() {
+
+ // Map option
+  var options = {
+    center: {lat: 38.693340 , lng: -9.351400},
+    zoom: 12
+  }
+
+  map = new google.maps.Map(document.getElementById("map"),options)
+
+}
+
+//Marker
+
+const marker = new google.maps.Marker({
+  position:{lat: 38.69105, lng: -9.32215},
+  map:map
+});
+
+
 /*-----------------------*/
 
